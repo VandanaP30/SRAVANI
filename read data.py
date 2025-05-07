28abaf8223dde0df413a878f851bdef762730c58
SRAVANI <vandanapisike2007@gmail.com>
2025-05-07 09:58:37 +0530

    Add files via upload

---
+++ read data.py
@@ -0,0 +1,9 @@
+#3.Defining and Using Functions
+#a)Write a function to read data from a file and display it on the screen
+a=str(input("Enter the name of the file with .txt extension:"))
+file2=open(a,'r')
+line=file2.readline()
+while(line!=""):
+ print(line)
+ line=file2.readline()
+file2.close()
